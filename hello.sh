56c6691c11cc52506c6d10f1ff5cffecd0697517
Meiviezhi P <meiviezhipadmanaban94@gmail.com>
2026-06-20 07:06:14 +0000

    added hello.sh

---
+++ hello.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "====================================="
+echo "Welcome to Jenkins CI/CD"
+echo "====================================="
+
+echo "Current Date:"
+date
+
+echo ""
+
+echo "Current User:"
+whoami
+
+echo ""
+
+echo "Current Directory:"
+pwd
+
+echo ""
+
+echo "Files:"
+ls -lrt
+
+echo ""
+
+echo "Script executed successfully!"
